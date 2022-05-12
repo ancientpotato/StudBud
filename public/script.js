c6182d06ffce182cbadfaee67fbb15094d75d56e
ancientpotato <100391334+ancientpotato@users.noreply.github.com>
2022-05-12 23:26:06 +1000

    creating a more refined side nav bar

---
+++ public/script.js
@@ -0,0 +1,12 @@
+function openTimer() {
+    document.getElementById("timer").style.display = "block";
+}
+
+function openGroup() {
+    document.getElementById("groupForm").style.display = "block";
+}
+
+function closeGroup() {
+    document.getElementById("groupForm").style.display = "none";
+}
+
