faecbc05935f167111aff527d36201ba4fa2add0
ancientpotato <100391334+ancientpotato@users.noreply.github.com>
2022-06-02 22:45:08 +1000

    added timer functionality

--- public/resourceExpanded.js
+++ public/resourceExpanded.js
@@ -1,1 +1,7 @@
+var expandCard = document.getElementsByClassName('card');
+var expandBtn = document.getElementsByClassName('viewMore');
+
+function viewResourceLinks() {
+    expandCard.style.width = "100%";
+}
 
--- public/timer.js
+++ public/timer.js
@@ -1,13 +1,29 @@
-//Timer
-
 //Code inspired from Florin Pop (referenced in markdown)
 var initialTimeInMins = 25;
-var time = initialTimeInMins * 60;
+var time;
+var timer;
+var isTimerRunning = false; //initially set to false since no timer is running 
 
+//grabbing the timer elements
 var timerCountdown = document.getElementById('countdownTimer');
 var countdownTimerModal = document.getElementById('countdownTimerModal');
+var startBtn = document.getElementById('timerBtn');
 
-var timer = setInterval(updateCountdown, 1000);
+function startTimer() {
+    //the timer starts/restarts when button is clicked
+    if (isTimerRunning) {
+        isTimerRunning = false;
+        startBtn.innerHTML = "Start";
+        clearInterval(timer);
+
+    } else {
+        //when clicked, button will change and timer stops
+        isTimerRunning = true;
+        startBtn.innerHTML = "Stop";
+        time = (initialTimeInMins * 60) - 1;
+        timer = setInterval(updateCountdown, 1000);
+    }
+}
 
 function updateCountdown() {
     var minutes = Math.floor(time / 60);
